5f2ab0640c1f4bbd11deb8d674615c96874d5321
Jeremiasz Mazur <jeremiasz.mazur@movity.pl>
2018-07-25 19:33:25 +0200

    add keymap for animation

--- Assets/Scripts/Ludek.cs
+++ Assets/Scripts/Ludek.cs
@@ -13,9 +13,13 @@ public class Ludek : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown("1"))
-        {
+        if (Input.GetKeyDown("1")){
+            anim.Play("Ludek Walk", -1, 0f);
+        }else if (Input.GetKeyDown("2")){
+            anim.Play("Ludek atak", -1, 0f);
+        }else if (Input.GetKeyDown("3")){
             anim.Play("Ludek skok", -1, 0f);
         }
-	}
+
+    }
 }
